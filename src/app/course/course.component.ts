39bd6a4d7021854b2d273f555d9495b1aa054857
QuimDJ <ebrinachain@email.com>
2023-12-15 20:14:06 +0100

    selected checkbox with no connex to paginator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,6 +8,7 @@ import { Lesson } from '../model/lesson';
 import {CoursesService} from "../services/courses.service";
 import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
 import {merge, fromEvent, throwError} from "rxjs";
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 @Component({
@@ -26,10 +27,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort)
   sort: MatSort;
 
-
-
   lessons: Lesson[] = [];
-  displayedColumns = ['seqNo', 'description', 'duration'];
+  
+  selection = new SelectionModel(true, []);
+
+  displayedColumns = ['select', 'seqNo', 'description', 'duration'];
+  expandedLesson: Lesson = null;
   
     constructor(private route: ActivatedRoute,
                 private coursesService: CoursesService) {
@@ -67,6 +70,34 @@ export class CourseComponent implements OnInit, AfterViewInit {
         .pipe(
           tap(() => this.loadLessonsPage())
         ).subscribe();
+  }
+  
+  onToggleLesson(lesson:Lesson) {
+    if (lesson == this.expandedLesson) {
+      this.expandedLesson = null;
     }
+    else {
+      this.expandedLesson = lesson;
+    }
+  }
+
+  onLessonToggled(lesson:Lesson) {
+    this.selection.toggle(lesson);
+    console.log(this.selection.selected);
+  }
+
+  isAllSelected() {
+    return this.selection.selected?.length == this.lessons?.length;
+  }
+
+  toggleAll() {
+    if (this.isAllSelected()) {
+      this.selection.clear();    
+    }
+    else {
+      // sprite operator converts array to sequence of numbers separated by commas 
+      this.selection.select(...this.lessons); 
+      }
+  }
 
 }
