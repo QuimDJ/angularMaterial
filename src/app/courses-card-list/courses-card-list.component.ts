4f7613161085c6ef882f0725af9ef101ca86c5b9
QuimDJ <ebrinachain@email.com>
2023-12-17 01:18:41 +0100

    dialog1

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
+import { DialogRef } from '@angular/cdk/dialog';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'courses-card-list',
@@ -12,7 +15,8 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor() {
+    constructor(private dialog: MatDialog) {
+        
     }
 
     ngOnInit() {
@@ -20,8 +24,13 @@ export class CoursesCardListComponent implements OnInit {
     }
 
     editCourse(course:Course) {
-
-
+        openEditCourseDialog(this.dialog, course)
+            .pipe(
+                filter((val:any) => !!val)
+            )
+            .subscribe(
+                (val: any) => console.log("new course value", val)
+            );
     }
 
 }
